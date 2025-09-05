c094b8f140f64f1856fd52df60c9748c407d0850
Ivan Arefkov <ivanarefkov@gmail.com>
2025-09-05 16:47:10 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, Query
+from pydantic import BaseModel
+from typing import Annotated
+from pydantic import AfterValidator
+
+app = FastAPI()
+
+class User(BaseModel):
+    name: str
+    id: int
+
+def validate_name_length(obj:User):
+    if len(obj.name) < 4:
+        raise ValueError('Name is too short')
+    return obj
+
+@app.get("/")
+async def root():
+    return {'message':'hello world'}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+@app.post("/post-test")
+async def post_req(user: Annotated[ User , AfterValidator(validate_name_length) ]):
+    return user
