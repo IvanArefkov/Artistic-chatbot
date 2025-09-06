35fe37dc57faf18b2be032be014d07dde5c310dc
Ivan Arefkov <ivanarefkov@gmail.com>
2025-09-05 17:07:47 -0700

    ruff linter setup

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import Annotated
 from pydantic import AfterValidator
 
 app = FastAPI()
 
+
 class User(BaseModel):
     name: str
     id: int
 
-def validate_name_length(obj:User):
+
+def validate_name_length(obj: User):
     if len(obj.name) < 4:
-        raise ValueError('Name is too short')
+        raise ValueError("Name is too short")
     return obj
+
 
 @app.get("/")
 async def root():
-    return {'message':'hello world'}
+    return {"message": "hello world"}
+
 
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
 
+
 @app.post("/post-test")
-async def post_req(user: Annotated[ User , AfterValidator(validate_name_length) ]):
+async def post_req(user: Annotated[User, AfterValidator(validate_name_length)]):
     return user
